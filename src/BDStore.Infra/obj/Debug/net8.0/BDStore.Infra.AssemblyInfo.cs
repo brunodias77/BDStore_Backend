b796b00d9879088c17a181d6127fe20d806ea9f9
brunodias77 <brunohdias95@gmail.com>
2024-01-24 18:52:24 -0300

    feat

--- src/BDStore.Infra/obj/Debug/net8.0/BDStore.Infra.AssemblyInfo.cs
+++ src/BDStore.Infra/obj/Debug/net8.0/BDStore.Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BDStore.Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f448f3d7297a737e95d386bdd9f6858608b8addd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BDStore.Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BDStore.Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
